d31fa6d2e788d2c2eddb94213631b6fe0a583e2a
Gambitoo <jroxo.salvado@gmail.com>
2023-05-17 00:05:00 +0100

    Login & Registo

--- Client/client.py
+++ Client/client.py
@@ -1,8 +1,51 @@
 import requests
-from cryptography.hazmat.primitives.asymmetric import ec,padding
-from cryptography.hazmat.primitives import serialization,hashes
+from OpenSSL import crypto
+from cryptography.hazmat.primitives.asymmetric import ec, padding
+from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.backends import default_backend
-import json
+from tkinter import *
+import tkinter.messagebox as tkMessageBox
+import os
+
+path = "client_keys"
+
+isExist = os.path.exists(path)
+if not isExist:
+   os.makedirs(path)
+
+def gen_client_rsa_keys():
+    private_key = crypto.PKey()
+    private_key.generate_key(crypto.TYPE_RSA, 2048)
+
+    # Write the private key to a PEM file
+    with open(os.path.join(path, 'client_private_key.pem'), 'wb') as private_key_file:
+        private_key_file.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, private_key))
+
+    # Extract the public key from the private key
+    public_key = private_key.to_cryptography_key().public_key()
+
+    # Write the public key to a PEM file
+    with open(os.path.join(path, 'client_public_key.pem'), 'wb') as public_key_file:
+        public_key_file.write(public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ))
+
+root = Tk()
+root.title("TUDOIGUAL - Sistema de Sincronização de Ficheiros Seguro")
+
+width = 640
+height = 480
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width / 2) - (width / 2)
+y = (screen_height / 2) - (height / 2)
+root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+root.resizable(0, 0)
+
+# =======================================VARIABLES=====================================
+credentials = {'username': StringVar(), 'password': StringVar()}
+
 
 # Load the public key from file
 with open('server-rsa-pk/public_key.pem', 'rb') as key_file:
@@ -11,68 +54,76 @@ with open('server-rsa-pk/public_key.pem', 'rb') as key_file:
         backend=default_backend()
     )
 
-#parametros para registo
-username = "DiogoV"
-password = "teste456"
-path = "myDir"
+def Exit():
+    result = tkMessageBox.askquestion('System', 'Are you sure you want to exit?', icon="warning")
+    if result == 'yes':
+        root.destroy()
+        exit()
 
-#encryptar dados com pk do server
-encrypted_username = public_key.encrypt(
-    username.encode('utf-8'), #para encryptar dados tem de ser binario com utf-8
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
+def LoginForm():
+    global LoginFrame, lbl_result1
+    LoginFrame = Frame(root)
+    LoginFrame.pack(side=TOP, pady=80)
+    lbl_username = Label(LoginFrame, text="Username:", font=('arial', 25), bd=18)
+    lbl_username.grid(row=1)
+    lbl_password = Label(LoginFrame, text="Password:", font=('arial', 25), bd=18)
+    lbl_password.grid(row=2)
+    lbl_result1 = Label(LoginFrame, text="", font=('arial', 18))
+    lbl_result1.grid(row=3, columnspan=2)
+    USERNAME = Entry(LoginFrame, font=('arial', 20), textvariable=credentials['username'], width=15)
+    USERNAME.grid(row=1, column=1)
+    PASSWORD = Entry(LoginFrame, font=('arial', 20), textvariable=credentials['password'], width=15, show="*")
+    PASSWORD.grid(row=2, column=1)
+    btn_login = Button(LoginFrame, text="Login", font=('arial', 18), width=35, command=Login)
+    btn_login.grid(row=4, columnspan=2, pady=20)
+    lbl_register = Label(LoginFrame, text="Register", fg="Blue", font=('arial', 12))
+    lbl_register.grid(row=0, sticky=W)
+    lbl_register.bind('<Button-1>', ToggleToRegister)
 
-encrypted_password = public_key.encrypt(
-    password.encode('utf-8'),
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
+def RegisterForm():
+    global RegisterFrame, lbl_result2
+    RegisterFrame = Frame(root)
+    RegisterFrame.pack(side=TOP, pady=40)
+    lbl_username = Label(RegisterFrame, text="Username:", font=('arial', 18), bd=18)
+    lbl_username.grid(row=1)
+    lbl_password = Label(RegisterFrame, text="Password:", font=('arial', 18), bd=18)
+    lbl_password.grid(row=2)
+    lbl_result2 = Label(RegisterFrame, text="", font=('arial', 18))
+    lbl_result2.grid(row=5, columnspan=2)
+    USERNAME = Entry(RegisterFrame, font=('arial', 20), textvariable=credentials['username'], width=15)
+    USERNAME.grid(row=1, column=1)
+    PASSWORD = Entry(RegisterFrame, font=('arial', 20), textvariable=credentials['password'], width=15, show="*")
+    PASSWORD.grid(row=2, column=1)
+    btn_login = Button(RegisterFrame, text="Register", font=('arial', 18), width=35, command=Register)
+    btn_login.grid(row=6, columnspan=2, pady=20)
+    lbl_login = Label(RegisterFrame, text="Login", fg="Blue", font=('arial', 12))
+    lbl_login.grid(row=0, sticky=W)
+    lbl_login.bind('<Button-1>', ToggleToLogin)
 
-encrypted_path = public_key.encrypt(
-    path.encode('utf-8'),
-    padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None
-    )
-)
+def ToggleToLogin(event=None):
+    RegisterFrame.destroy()
+    LoginForm()
 
-# a encriptaçao cria os dados em binario com latin-1, e dá erro ao receber no server com este formato por isso dou decode antes de enviar para enviar a cifra com "string" em vez de "binario"
-parameters = {'key1': encrypted_username.decode('latin-1'), 'key2': encrypted_password.decode('latin-1'), 'key3':encrypted_path.decode('latin-1')}
+def ToggleToRegister(event=None):
+    LoginFrame.destroy()
+    RegisterForm()
 
+def Login():
+    url = 'http://127.0.0.1:5000/createNonce'
+    parameters = {'key1': credentials['username'].get()}
+    response = requests.post(url, data=parameters)
 
-#descomentar para testar registo
-'''url = 'http://127.0.0.1:5000/regist'
+    nonce = response.text.encode('utf-8')
 
-#enviar ficheiro da pk do user
-file_path = 'server-rsa-pk/public_key.pem' #neste caso nao estou a enviar do user porque ainda não existe, mas aqui é para substituir por pk do user
-files = {'file': open(file_path, 'rb')}
-response = requests.post(url, files=files, data=parameters)'''
-
-#descomentar para testar authenticação
-'''
-url = 'http://127.0.0.1:5000/authNonce'
-parameters = {'key1': "DiogoV"}
-response = requests.post(url, data=parameters)
-
-print(response.text)
-
-with open('../Server/RSA-keys/private_key.pem', 'rb') as key_file:
+    with open('client_keys/client_private_key.pem', 'rb') as key_file:
         private_key = serialization.load_pem_private_key(
             key_file.read(),
             password=None,
             backend=default_backend()
         )
 
-signature = private_key.sign(
-        response.text.encode('utf-8'),
+    signature = private_key.sign(
+        nonce,
         padding.PSS(
             mgf=padding.MGF1(hashes.SHA256()),
             salt_length=padding.PSS.MAX_LENGTH
@@ -80,10 +131,63 @@ signature = private_key.sign(
         hashes.SHA256()
     )
 
-url = 'http://127.0.0.1:5000/auth'
-parameters = {'key1':signature.decode('latin-1'),'key2': "DiogoV"}
-response = requests.post(url, data=parameters)
+    url = 'http://127.0.0.1:5000/auth'
+    parameters = {'key1': signature.decode('latin-1'), 'key2': credentials['username'].get()}
+    response = requests.post(url, data=parameters)
 
-print(response.text)'''
+    print(response.text)
 
+def Register():
+    # encryptar dados com pk do server
+    gen_client_rsa_keys()
+    filePath = "fileDir"
 
+    encrypted_username = public_key.encrypt(
+        credentials['username'].get().encode('utf-8'),  # para encryptar dados tem de ser binario com utf-8
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    encrypted_password = public_key.encrypt(
+        credentials['password'].get().encode('utf-8'),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    encrypted_path = public_key.encrypt(
+        filePath.encode('utf-8'),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    # a encriptaçao cria os dados em binario com latin-1, e dá erro ao receber no server com este formato por isso dou decode antes de enviar para enviar a cifra com "string" em vez de "binario"
+    parameters = {'key1': encrypted_username.decode('latin-1'), 'key2': encrypted_password.decode('latin-1'),
+                  'key3': encrypted_path.decode('latin-1')}
+
+    url = 'http://127.0.0.1:5000/registo'
+
+    # enviar ficheiro da pk do user
+    file_path = os.path.join(path, 'client_public_key.pem') # neste caso nao estou a enviar do user porque ainda não existe, mas aqui é para substituir por pk do user
+    files = {'file': open(file_path, 'rb')}
+    response = requests.post(url, files=files, data=parameters)
+
+    print(response.text)
+
+LoginForm()
+
+# ========================================MENUBAR WIDGETS==================================
+menubar = Menu(root)
+root.config(menu=menubar)
+
+# ========================================INITIALIZATION===================================
+if __name__ == '__main__':
+    root.mainloop()
--- Server/server.py
+++ Server/server.py
@@ -3,14 +3,12 @@ import mysql.connector
 from concurrent.futures import ThreadPoolExecutor
 import bcrypt
 from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import serialization,hashes
+from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.backends import default_backend
 import secrets
 import json
 
-
 app = Flask(__name__)
-
 
 # create a database connection
 mydb = mysql.connector.connect(
@@ -23,9 +21,9 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 executor = ThreadPoolExecutor()
 
-def auth_user(signature,name):
 
-    #ir buscar o nome da public key do user à bd
+def auth_user(signature, name):
+    # ir buscar o nome da public key do user à bd
     sql = "SELECT public_key FROM users WHERE username = %s"
     val = (name,)
     mycursor.execute(sql, val)
@@ -36,23 +34,22 @@ def auth_user(signature,name):
     if result is not None:
         pk_name = result[0]
 
-        #abrir pk do user da diretoria client_keys
-        with open('client_keys/'+pk_name, 'rb') as key_file:
+        # abrir pk do user da diretoria client_keys
+        with open('client_keys/' + pk_name, 'rb') as key_file:
             public_key = serialization.load_pem_public_key(
                 key_file.read(),
                 backend=default_backend()
             )
-            
 
-        #abrir e ler o nonce criado para a autenticação desse user da diretoria auth_nonce
-        with open('auth_nonce/nonce'+name+'.txt', 'r') as file:
+        # abrir e ler o nonce criado para a autenticação desse user da diretoria auth_nonce
+        with open('auth_nonce/nonce' + name + '.txt', 'r') as file:
             # Read the contents of the file
             contents = file.read()
 
         # Convert the contents to an integer
         nonce = contents
-        
-        #verificar assinatura com pk do user
+
+        # verificar assinatura com pk do user
         try:
             public_key.verify(
                 signature,
@@ -63,67 +60,67 @@ def auth_user(signature,name):
                 ),
                 hashes.SHA256()
             )
-            return "autenticado"  # Signature is valid
+            return "Autenticado. Bem-vindo!"  # Signature is valid
         except Exception as e:
             return str(e)  # Signature is invalid
 
     else:
         return "0"
-    
 
-#função para guardar registo na bd
-def register_user(name, password,pk,path):
 
+# função para guardar registo na bd
+def register_user(name, password, pk, path):
     sql = "SELECT COUNT(*) AS user_count FROM users WHERE username = %s"
     val = (name,)
     mycursor.execute(sql, val)
     result = mycursor.fetchone()
-    
+
     if result[0] == 0:
-        #criar salt para guardar representação de bd
+        # criar salt para guardar representação de bd
         salt = bcrypt.gensalt()
         hashed_password = bcrypt.hashpw(password.encode(), salt)
 
         sql = "INSERT INTO users (username, password, public_key, directory_path) VALUES (%s, %s,%s,%s)"
-        val = (name, hashed_password.decode(),pk,path)
+        val = (name, hashed_password.decode(), pk, path)
         mycursor.execute(sql, val)
         mydb.commit()
-        
-        if mycursor.rowcount == 1:
-            return "1"
-        else:
-            return "0"
-    else:
-        return "0"
 
-    
-#endpoint para autenticar (recebe username e assinatura do nonce)
+        if mycursor.rowcount == 1:
+            return "Registo bem sucedido!"
+        else:
+            return "Erro. Registo não efetuado."
+    else:
+        return "Utilizador já existente."
+
+
+# endpoint para autenticar (recebe username e assinatura do nonce)
 @app.route('/auth', methods=['POST'])
 def auth():
     data = request.form
-    
-    result = auth_user(data['key1'].encode('latin-1'),data['key2'])
-    return result   
 
-#endpoint para receber nonce de autenticação (recebe o username)
-@app.route('/authNonce', methods=['POST'])
+    result = auth_user(data['key1'].encode('latin-1'), data['key2'])
+    return result
+
+
+# endpoint para receber nonce de autenticação (recebe o username)
+@app.route('/createNonce', methods=['POST'])
 def authNonce():
     data = request.form
     random_number = secrets.randbits(128)
 
-    with open('auth_nonce/nonce'+data['key1']+'.txt', 'w') as file:
+    with open('auth_nonce/nonce' + data['key1'] + '.txt', 'w') as file:
         # Write the random number to the file
         file.write(str(random_number))
 
-
     return str(random_number)
 
-@app.route('/regist', methods=['POST'])
+
+@app.route('/registo', methods=['POST'])
 def register():
-    #receber dados
+    # receber dados
     data = request.form
 
-    #abrir sk do server
+    # abrir sk do server
     with open('RSA-keys/private_key.pem', 'rb') as key_file:
         private_key = serialization.load_pem_private_key(
             key_file.read(),
@@ -131,9 +128,9 @@ def register():
             backend=default_backend()
         )
 
-    #desencryptar com sk do server
+    # desencryptar com sk do server
     username = private_key.decrypt(
-        data['key1'].encode('latin-1'), # e temos de voltar a codificar para "binario" com latin-1 
+        data['key1'].encode('latin-1'),  # e temos de voltar a codificar para "binario" com latin-1
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
@@ -159,16 +156,17 @@ def register():
         )
     )
 
-    #receber o pk do user
+    # receber o pk do user
     file = request.files['file']
-    #o nome do ficheiro fica usernamepk.pem
-    filename = username.decode('utf-8')+'pk.pem'
-    #vai para a diretoria do server, na subdiretoria client_keys
-    file.save('client_keys/'+filename)
-    
-    #enviar os dados para a função que guarda os dados na bd
-    result = register_user(username.decode('utf-8'), password.decode('utf-8'),filename,path.decode('utf-8'))
+    # o nome do ficheiro fica usernamepk.pem
+    filename = username.decode('utf-8') + 'pk.pem'
+    # vai para a diretoria do server, na subdiretoria client_keys
+    file.save('client_keys/' + filename)
+
+    # enviar os dados para a função que guarda os dados na bd
+    result = register_user(username.decode('utf-8'), password.decode('utf-8'), filename, path.decode('utf-8'))
     return result
+
 
 if __name__ == '__main__':
     app.run(debug=True)
