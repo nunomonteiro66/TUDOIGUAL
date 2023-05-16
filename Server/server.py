775110b78fa349a8fb6edefac4ffc5576e3b1b33
DESKTOP-RRKKBQQ\Dmrv2 <diogo.ventura@segal.ubi.pt>
2023-05-16 11:50:24 +0100

    funções para autenticação ainda por testar

--- Server/server.py
+++ Server/server.py
@@ -23,16 +23,48 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 executor = ThreadPoolExecutor()
 
-def auth_user(signature,nonce):
+def auth_user(signature,name):
 
+    sql = "SELECT public_key FROM `users` WHERE username = %s"
+    val = (name,)
+    mycursor.execute(sql, val)
+
+    if mycursor.rowcount == 1:
+        result = mycursor.fetchone()
+        pk_name = result[0]
+
+        with open('client_keys/'+pk_name+'.pem', 'rb') as key_file:
+            public_key = serialization.load_pem_private_key(
+                key_file.read(),
+                password=None,
+                backend=default_backend()
+            )
+
+        with open('auth_nonce/nonce'+name+'random_number.txt', 'r') as file:
+            # Read the contents of the file
+            contents = file.read()
+
+        # Convert the contents to an integer
+        nonce = int(contents)
+        
+        try:
+            public_key.verify(
+                signature,
+                nonce,
+                padding.PKCS1v15(),
+                public_key.signing_algorithm
+            )
+            return True  # Signature is valid
+        except Exception:
+            return False  # Signature is invalid
+
+    else:
+        return "0"
     
-
-    return "0"
 
 #função para guardar registo na bd
 def register_user(name, password,pk,path):
 
-    print(name)
     sql = "SELECT COUNT(*) AS user_count FROM users WHERE username = %s"
     val = (name,)
     mycursor.execute(sql, val)
@@ -56,14 +88,27 @@ def register_user(name, password,pk,path):
         return "0"
 
     
-    
+#endpoint para autenticar (recebe username e assinatura do nonce)
 @app.route('/auth', methods=['POST'])
 def auth():
     data = request.form
     
     
-    result = auth_user("ola")
+    result = auth_user(data['key1'],data['key2'])
     return result   
+
+#endpoint para receber nonce de autenticação (recebe o username)
+@app.route('/authNonce', methods=['POST'])
+def auth():
+    data = request.form
+    random_number = secrets.randbits(128)
+
+    with open('nonce'+data['key1']+'.txt', 'w') as file:
+        # Write the random number to the file
+        file.write(str(random_number))
+
+
+    return random_number
 
 @app.route('/regist', methods=['POST'])
 def register():
