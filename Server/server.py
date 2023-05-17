1111dcbba31b09ae5fd0fddff00330518e92239f
DESKTOP-PNR8IDI\DIogo <diogo.ventura@segal.ubi.pt>
2023-05-17 19:15:23 +0100

    Criação de endpoints para enviar file para servidor e verificar sincronização do cliente

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, send_from_directory
 import mysql.connector
 from concurrent.futures import ThreadPoolExecutor
 import bcrypt
@@ -6,8 +6,8 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.backends import default_backend
 import secrets
-import json
 from cryptography.hazmat.primitives.asymmetric import ec
+import os
 
 app = Flask(__name__)
 
@@ -21,6 +21,95 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 executor = ThreadPoolExecutor()
+
+# endpoint para verificar ficheiros em falta para cliente: necessita de username
+
+
+@app.route('/checkFiles', methods=['POST'])
+def checkFiles():
+    data = request.form
+
+    # Open the file in read mode
+    file = open("clien_files/"+data['key1']+".txt", 'r')
+
+    # Read the lines of the file into a list
+    file_lines = file.readlines()
+
+    # Close the file
+    file.close()
+    # Process the lines and create a list of filenames
+    filenames = [line.strip() for line in file_lines]
+
+    if len(filenames) == 0:
+        return "está sync"
+    else:
+        # limpar o ficheiro porque o cliente vai ficar sincronizado
+        file = open("clien_files/"+data['key1']+".txt", 'w')
+        file.close()
+        responses = []
+        for file_path in filenames:
+            response = send_from_directory(
+                '/path/to/directory', file_path, as_attachment=True)
+            responses.append(response)
+
+            # Combine the responses into a single response
+            custom_response = app.make_response(responses)
+
+    return custom_response
+
+
+def sign_file(hash):
+
+    with open('client_keys/client_private_key.pem', 'rb') as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
+
+    # Sign the message with the private key
+    signature = private_key.sign(
+        hash.encode('utf-8'),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    # Return the signature as bytes
+    return signature
+
+# endpoint para enviar ficheiros do cliente para o servidor: necessita de filename, file, hash do file, chave para desencriptar por esta ordem, username
+
+
+@app.route('/sendFile', methods=['POST'])
+def sendFile():
+    data = request.form
+
+    file = request.files['file']
+
+    # é necessário verificar o hash do ficheiro
+
+    # é necessario calcular a assinatura do ficheiro pelo hash
+    signature = sign_file(data['key2'])
+    # Save the file to a desired location on the server
+    file.save('files/'+data['key1'])
+
+    # colocar ficheiro em falta para todos os users excepto no user que coloca o file
+
+    for filename in os.listdir("client_files"):
+        # Create the full file path
+        file_path = os.path.join("client_files", filename)
+
+        # Check if the path corresponds to a file
+        if os.path.isfile(file_path) and file_path not in data['key4']+'.txt':
+            # Open the file in append mode ('a')
+            with open(file_path, 'a') as file:
+                # Write the line to the file
+                file.write(data['key1'] + "\n")
+
+    return str(signature)
 
 
 def auth_user(signature, name):
@@ -55,8 +144,8 @@ def auth_user(signature, name):
             public_key.verify(
                 signature,
                 nonce.encode('utf-8'),
-                #padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),
-                #hashes.SHA256()
+                # padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),
+                # hashes.SHA256()
                 ec.ECDSA(hashes.SHA256())
             )
             return "Autenticado. Bem-vindo!"  # Signature is valid
@@ -85,6 +174,8 @@ def register_user(name, password, pk, path):
         mydb.commit()
 
         if mycursor.rowcount == 1:
+            file = open("client_files/"+name+".txt", 'w')
+            file.close()
             return "Registo bem sucedido!"
         else:
             return "Erro. Registo não efetuado."
@@ -129,7 +220,8 @@ def register():
 
     # desencryptar com sk do server
     username = private_key.decrypt(
-        data['key1'].encode('latin-1'),  # e temos de voltar a codificar para "binario" com latin-1
+        # e temos de voltar a codificar para "binario" com latin-1
+        data['key1'].encode('latin-1'),
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
@@ -163,7 +255,8 @@ def register():
     file.save('client_keys/' + filename)
 
     # enviar os dados para a função que guarda os dados na bd
-    result = register_user(username.decode('utf-8'), password.decode('utf-8'), filename, path.decode('utf-8'))
+    result = register_user(username.decode(
+        'utf-8'), password.decode('utf-8'), filename, path.decode('utf-8'))
     return result
 
 
